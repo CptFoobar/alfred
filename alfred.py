cd4b7869256b84f3f673d70565a782e314c8d03b
Sidhant Sharma [:tk] <tigerkid001@gmail.com>
2016-03-31 16:05:33 +0530

    Added support for paths

--- alfred.py
+++ alfred.py
@@ -2,12 +2,14 @@
 
 import argparse
 import BaseHTTPServer
+import ntpath
 import os
 import SimpleHTTPServer
 import socket
 import SocketServer
 import sys
 from netifaces import interfaces, ifaddresses, AF_INET
+from time import gmtime, strftime
 
 # Class to put ThreadingMixIn and HTTPServer together
 class ThreadingSimpleServer(SocketServer.ThreadingMixIn,
@@ -69,6 +71,51 @@ def threadedServer(port, directory, max_serve_count, force_port):
         print("\rShutting down the server.")
 
 
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+def main(args):
+    dirname = os.getcwd()
+    serving_file = False
+    origin_dir = os.getcwd()
+
+    if args.path and len(args.path) > 0:
+        file_to_serve = ''
+        if (os.path.isdir(args.path)):
+            # Serving directory that is not cwd
+            dirname = args.path
+        elif (args.path in os.listdir(os.getcwd())):
+            # Serving file from current directory
+            file_to_serve = os.path.join(os.getcwd(), args.path)
+            dirname = "alfred_" + strftime("%Y_%m_%d_%H_%M", gmtime())
+            os.mkdir(dirname)
+            os.symlink(file_to_serve, os.path.join(dirname, args.path))
+            serving_file = True
+        elif (os.path.isfile(args.path)):
+            # Serving file in another directory
+            file_to_serve = args.path
+            dirname = os.path.join(os.getcwd(), "alfred_" + strftime("%Y_%m_%d_%H_%M", gmtime()))
+            os.mkdir(dirname)
+            os.symlink(file_to_serve, os.path.join(dirname, path_leaf(args.path)))
+            serving_file = True
+        else:
+            print "Bad file path. Exiting."
+            return -1
+
+    # Start server
+    threadedServer(args.port, dirname, args.count_max, args.force_port)
+
+    #Cleanup
+    if serving_file:
+        os.chdir(origin_dir)
+        for file_name in os.listdir(dirname):
+            os.remove(os.path.join(dirname, file_name))
+        os.rmdir(dirname)
+
+    return 0
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -79,10 +126,6 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", type=int, default=8021,
                         help="specify port to host server on (default: 8021)")
 
-    # option for setting directory
-    parser.add_argument("-d", "--directory", type=str,
-                        help="specify directory to be served (default: current directory)")
-
     # option for setting maximum serve count
     parser.add_argument("-c", "--count-max", type=int, default=10000,
                         help="maximum number of times directory can be served (default: 10000)")
@@ -91,7 +134,11 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--force-port", action="store_true",
                         help="force the server to run on specified port (server won't start if port unavailable)")
 
+    # option for serving only a particular file
+    parser.add_argument("-P", "--path", type=str,
+                        help="specify path (file or directory) to be served (overrides --directory)")
+
     args = parser.parse_args()
 
-    # Start server
-    threadedServer(args.port, args.directory, args.count_max, args.force_port)
+    ret = main(args)
+    print "Return status: " + str(ret)
